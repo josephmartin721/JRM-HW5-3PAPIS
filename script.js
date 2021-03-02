28d5c12bff296443de965b1448c239c46379cc31
Joseph Martin <josephmartin721@yahoo.com>
2021-03-02 11:33:07 -0600

    page now able to store locally

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
     
-    $("#saveBtn").on("click", function () {
+    $(".btn").on("click", function () {
        
-        replaceTextarea.log(this);
+        console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
-    });
+    })
+    $("#time8 .description").val(localStorage.getItem("time8"));
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
+    $("#time11 .description").val(localStorage.getItem("time11"));
+    $("#timenoon .description").val(localStorage.getItem("timenoon"));
+    $("#oneafternoon .description").val(localStorage.getItem("oneafternoon"));
+    $("#twoafternoon .description").val(localStorage.getItem("twoafternoon"));
+    $("#threeafternoon .description").val(localStorage.getItem("threeafternoon"));
+    $("#fourafternoon .description").val(localStorage.getItem("fourafternoon"));
 
     function hourTracker() {
         var currentHour = moment().hour();
 
-        $("#time-block").each(function () {
+        $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
             console.log( blockHour, currentHour)
 
@@ -34,4 +43,5 @@ $(document).ready(function () {
             }
         })
     }
+    hourTracker();
 });
